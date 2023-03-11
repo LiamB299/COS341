206423af2541ac77957b244e5a82194ae98ca5a6
liam.burgess <liam.burgess@labs.epiuse.com>
2023-03-11 12:58:04 +0200

    remove no move state function

--- Assignment 1/lexer/lexer.py
+++ Assignment 1/lexer/lexer.py
@@ -81,7 +81,7 @@ def add_dot(regex):
 
     for i, char in enumerate(regex):
         if char == '(' and i != 0:
-            if regex[i-1] in "|(":
+            if regex[i - 1] in "|(":
                 output += "("
             else:
                 output += '.('
@@ -90,11 +90,10 @@ def add_dot(regex):
             output += char
             continue
         elif char.isalnum() and i != 0:
-            if regex[i-1] not in "(|":
+            if regex[i - 1] not in "(|":
                 output += '.'
         output += char
     return output
-
 
 
 # input_table = ""
@@ -150,11 +149,20 @@ def build_intro_nfa_table(expression):
     return count
 
 
-def kleen_star(nfa_block: _nfa_block, state_count: int):
-    new_starting = {"state": state_count+1,"input": ["#"], "next_state": [state_count+2], "accepting": False, "starting": True}
-    new_finishing = {"state": state_count+2, "input": ["#"], "next_state": [-1], "accepting": True, "starting": False}
-    state_count += 2
+def remove_no_move_state(nfa_block: _nfa_block):
+    for state in nfa_block['states']:
+        for i in range(0, len(state['input'])):
+            if state['next_state'][i] == -1 and state['input'][i] == "#" and state['accepting'] == False:
+                state['next_state'].pop(i)
+                state['input'].pop(i)
+                break
 
+
+def kleen_star(nfa_block: _nfa_block, state_count: int):
+    new_starting = {"state": state_count + 1, "input": ["#"], "next_state": [state_count + 2], "accepting": False,
+                    "starting": True}
+    new_finishing = {"state": state_count + 2, "input": ["#"], "next_state": [-1], "accepting": True, "starting": False}
+    state_count += 2
 
     for state in nfa_block['states']:
         # link old finish to new finish
@@ -168,15 +176,6 @@ def kleen_star(nfa_block: _nfa_block, state_count: int):
                     state['input'].append("#")
                     state['next_state'].append(start['state'])
 
-            # remove no move state
-            remove_index = []
-            for i in range(len(state['next_state'])):
-                if state['next_state'][i] == -1:
-                    remove_index.append(i)
-            for i in range(len(remove_index) - 1, -1, -1):
-                state['next_state'].pop(remove_index[i])
-                state['input'].pop(remove_index[i])
-
         # link new starting
         if state['starting']:
             new_starting["input"].append("#")
@@ -188,7 +187,7 @@ def kleen_star(nfa_block: _nfa_block, state_count: int):
 
     nfa_block['states'].append(new_starting)
     nfa_block['states'].append(new_finishing)
-    nfa_block['nfa'] = "("+nfa_block['nfa']+"*)"
+    nfa_block['nfa'] = "(" + nfa_block['nfa'] + "*)"
     nfa_block['type'] = "expression"
     return nfa_block, state_count
 
@@ -207,15 +206,6 @@ def concatenate(nfa_block_a: _nfa_block, nfa_block_b: _nfa_block):
         if state_a["accepting"]:
             state_a["accepting"] = False
 
-            # remove no move state
-            remove_index = []
-            for i in range(len(state_a['next_state'])):
-                if state_a['next_state'][i] == -1:
-                    remove_index.append(i)
-            for i in range(len(remove_index) - 1, -1, -1):
-                state_a['next_state'].pop(remove_index[i])
-                state_a['input'].pop(remove_index[i])
-
     # remove starting states of B
     for state_b in nfa_block_b['states']:
         if state_b["starting"]:
@@ -227,9 +217,9 @@ def concatenate(nfa_block_a: _nfa_block, nfa_block_b: _nfa_block):
         states.append(state)
 
     return {
-        "nfa" : "("+nfa_block_a["nfa"]+"."+nfa_block_b["nfa"]+")",
-        "type" : "expression",
-        "states" : states
+        "nfa": "(" + nfa_block_a["nfa"] + "." + nfa_block_b["nfa"] + ")",
+        "type": "expression",
+        "states": states
     }
 
 
@@ -241,7 +231,7 @@ def question_mark(nfa_block: _nfa_block):
                     start_state['input'].append('#')
                     start_state['next_state'].append(fin_state['state'])
     nfa_block['type'] = 'expression'
-    nfa_block['nfa'] = "("+nfa_block['nfa']+'?)'
+    nfa_block['nfa'] = "(" + nfa_block['nfa'] + '?)'
     return nfa_block
 
 
@@ -266,15 +256,15 @@ def plus(nfa_block: _nfa_block, state_count: int):
     # add new state to block
     nfa_block['states'].append(new_starting)
 
-    nfa_block['nfa'] = "("+nfa_block['nfa']+"+)"
+    nfa_block['nfa'] = "(" + nfa_block['nfa'] + "+)"
     nfa_block['type'] = "expression"
     return nfa_block, state_count
 
 
 def orr(nfa_block_a: _nfa_block, nfa_block_b: _nfa_block, state_count: int):
     # create new starting and end state
-    new_start_state = {"state": state_count+1, "input": [], "next_state": [], "accepting": False, "starting": True}
-    end_state = {"state": state_count+2, "input": ["#"], "next_state": ["-1"], "accepting": True, "starting": False}
+    new_start_state = {"state": state_count + 1, "input": [], "next_state": [], "accepting": False, "starting": True}
+    end_state = {"state": state_count + 2, "input": ["#"], "next_state": [-1], "accepting": True, "starting": False}
     state_count += 2
 
     # link initial states to new start state
@@ -297,29 +287,11 @@ def orr(nfa_block_a: _nfa_block, nfa_block_b: _nfa_block, state_count: int):
             fin_state['next_state'].append(end_state['state'])
             fin_state['accepting'] = False
 
-            # remove no move state
-            remove_index = []
-            for i in range(len(fin_state['next_state'])):
-                if fin_state['next_state'][i] == -1:
-                    remove_index.append(i)
-            for i in range(len(remove_index) - 1, -1, -1):
-                fin_state['next_state'].pop(remove_index[i])
-                fin_state['input'].pop(remove_index[i])
-
     for fin_state in nfa_block_b['states']:
         if fin_state['accepting']:
             fin_state['input'].append("#")
             fin_state['next_state'].append(end_state['state'])
             fin_state['accepting'] = False
-
-            # remove no move state
-            remove_index = []
-            for i in range(len(fin_state['next_state'])):
-                if fin_state['next_state'][i] == -1:
-                    remove_index.append(i)
-            for i in range(len(remove_index) - 1, -1, -1):
-                fin_state['next_state'].pop(remove_index[i])
-                fin_state['input'].pop(remove_index[i])
 
     # generate new state block and return it
     states = nfa_block_a['states']
@@ -329,9 +301,9 @@ def orr(nfa_block_a: _nfa_block, nfa_block_b: _nfa_block, state_count: int):
     states.append(end_state)
 
     return {
-        "nfa" : "("+nfa_block_a["nfa"]+"|"+nfa_block_b["nfa"]+")",
-        "type" : "expression",
-        "states" : states
+        "nfa": "(" + nfa_block_a["nfa"] + "|" + nfa_block_b["nfa"] + ")",
+        "type": "expression",
+        "states": states
     }, state_count
 
 
@@ -341,12 +313,13 @@ def print_state_table(nfa):
         if nfa_block_internal['type'] == 'symbol':
             continue
         count += 1
-        print("\n\nblock:\t"+str(count))
+        print("\n\nblock:\t" + str(count))
         print("nfa\t\t\t\tstate\tinput\tnext_state\t\tstarting_state\t\taccepting_state")
         for states in nfa_block_internal['states']:
             for i in range(0, len(states['next_state'])):
                 print(nfa_block_internal['nfa'] + "\t\t\t\t" + str(states['state']) + "\t\t" + str(
-                    states['input'][i]) + "\t\t" + str(states['next_state'][i]) + "\t\t\t\t" + str(states['starting']) + '\t\t\t\t' + str(states['accepting']) + "\n")
+                    states['input'][i]) + "\t\t" + str(states['next_state'][i]) + "\t\t\t\t" + str(
+                    states['starting']) + '\t\t\t\t' + str(states['accepting']) + "\n")
 
 
 def parser(expression="00(01|10)*11*", validator="0.0.(0.1|1.0)*.1.1*"):
@@ -357,7 +330,7 @@ def parser(expression="00(01|10)*11*", validator="0.0.(0.1|1.0)*.1.1*"):
         return 0
 
     # convert to postfix
-    print("expression\t"+ expression)
+    print("expression\t" + expression)
     postfix = infix_to_postfix(expression)
     print("postfix\t" + postfix)
 
@@ -380,25 +353,25 @@ def parser(expression="00(01|10)*11*", validator="0.0.(0.1|1.0)*.1.1*"):
         symbol = stack[i]
         if symbol['nfa'] == "*":
             print("star")
-            new_block, count = kleen_star(stack[i-1], count)
+            new_block, count = kleen_star(stack[i - 1], count)
             stack.pop(i - 1)
             stack[i - 1] = new_block
             i = 0
         elif symbol['nfa'] == "?":
             print("question_mark")
-            new_block = question_mark(stack[i-1])
+            new_block = question_mark(stack[i - 1])
             stack.pop(i - 1)
             stack[i - 1] = new_block
             i = 0
         elif symbol['nfa'] == "+":
             print("plus")
-            new_block, count = plus(stack[i-1], count)
+            new_block, count = plus(stack[i - 1], count)
             stack.pop(i - 1)
-            stack[i-1] = new_block
+            stack[i - 1] = new_block
             i = 0
         elif symbol['nfa'] == ".":
             print("concat")
-            new_block = concatenate(stack[i-2], stack[i-1])
+            new_block = concatenate(stack[i - 2], stack[i - 1])
             stack.pop(i - 1)
             stack.pop(i - 1)
             stack[i - 2] = new_block
@@ -406,15 +379,16 @@ def parser(expression="00(01|10)*11*", validator="0.0.(0.1|1.0)*.1.1*"):
         elif symbol['nfa'] == "|":
             print("or")
             new_block, count = orr(stack[i - 2], stack[i - 1], count)
-            stack.pop(i-1)
-            stack.pop(i-1)
-            stack[i-2] = new_block
+            stack.pop(i - 1)
+            stack.pop(i - 1)
+            stack[i - 2] = new_block
             i = 0
+
+    # remove old no move states
+    remove_no_move_state(stack[0])
 
     # print final NFA
     print_state_table(stack)
 
 
 print(parser())
-
-
