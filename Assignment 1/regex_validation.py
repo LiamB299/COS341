2a8aa486b199c97b7809f27f480e356218649c83
liam.burgess <liam.burgess@labs.epiuse.com>
2023-03-18 13:26:52 +0200

    regex validation function

---
+++ Assignment 1/regex_validation.py
@@ -0,0 +1,65 @@
+def validate_expression(expression: str):
+    if expression == '':
+        return True
+
+    expr = expression.replace(" ", "")
+
+    # format symbols to $
+    expr = expr.replace("*", "$")
+    expr = expr.replace("+", "$")
+    expr = expr.replace("?", "$")
+
+    # replace chars
+    for i, char in enumerate(expr):
+        if char.isalnum():
+            expr = expr[:i] + '@' + expr[i + 1:]
+
+    changed = True
+    while changed:
+        old_expr = expr
+        expr = expr.replace('@$', 'R')
+        changed = not (old_expr == expr)
+
+    changed = True
+    while changed:
+        old_expr = expr
+        expr = expr.replace('@', 'R')
+        changed = not(old_expr == expr)
+
+    # apply recursion rules
+    changing = True
+    older_expression = ''
+    while changing:
+        older_expression = expr
+
+        changed = True
+        while changed:
+            old_expr = expr
+            expr = expr.replace('R|R', 'R')
+            changed = not (old_expr == expr)
+
+        changed = True
+        while changed:
+            old_expr = expr
+            expr = expr.replace('RR', 'R')
+            changed = not (old_expr == expr)
+
+        changed = True
+        while changed:
+            old_expr = expr
+            expr = expr.replace('(R)$', 'R')
+            changed = not (old_expr == expr)
+
+        changed = True
+        while changed:
+            old_expr = expr
+            expr = expr.replace('(R)', 'R')
+            changed = not (old_expr == expr)
+
+        changing = not(older_expression == expr)
+        older_expression = expr
+
+    return older_expression == 'R'
+
+
+print(validate_expression("(a*)*"))
